63b80068986a673ad62b0e96cc3abd7528f11f74
priyansh-garg1 <priyanshgarg382@gmail.com>
2024-12-21 11:42:58 +0530

    Updated

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 // export const MONGO_URI = 'mongodb+srv://priyanshgarg382:priyansh1090@cluster0.bwszc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-export const MONGO_URI = 'mongodb+srv://priyansh:priyansh@cluster0.uqvhp.mongodb.net/?retryWrites=true&w=majority';
+export const MONGO_URI = 'mongodb+srv://priyansh:priyansh@cluster0.uqvhp.mongodb.net/mydatabase?retryWrites=true&w=majority';
 export const APP_SECRET = '238745623hsdf'
 
 export const PORT = process.env.PORT || 8000;
